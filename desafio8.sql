13a3b27cffad6a8b5e194281110608aa8601417a
João <joaolelles7@gmail.com>
2023-02-02 17:37:11 -0300

    Requisito 8, Exibindo os albuns produzidos pela Elis Regina

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+    art.nome AS 'artista',
+    alb.nome_album AS 'album'
+FROM
+    SpotifyClone.artista AS art
+    INNER JOIN SpotifyClone.album AS alb ON art.artista_id = alb.artista_id
+
+WHERE
+    art.nome = 'Elis Regina'
+
+ORDER BY album
